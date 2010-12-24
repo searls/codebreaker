eb2e03fcfc2dbc702a041226c050f18daf550c33
Justin Searls <searls@gmail.com>
2010-12-24 11:07:55 -0500

    p. 143

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -4,9 +4,36 @@ module Codebreaker
       @output = output
     end
    
-    def start
+    def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
+    end
+    
+    def guess(guess)
+      mark = ''
+      (0..3).each do |index|
+        if exact_match?(guess,index)
+          mark << '+'
+        end
+      end
+      (0..3).each do |index|        
+        if number_match?(guess,index)
+          mark << '-'
+        end
+      end
+      puts mark
+      @output.puts mark
+    end
+    
+    private 
+
+    def exact_match?(guess,index)
+      guess[index] == @secret[index]
+    end
+    
+    def number_match?(guess,index)
+      @secret.include?(guess[index]) && !exact_match?(guess,index)
     end
   end
 end
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -5,9 +5,7 @@ module Codebreaker
     describe '#start' do
       let(:output) { gimme }
       let(:game) { Game.new(output) }
-      before do
-        game.start
-      end
+      before { game.start('1234') }
 
       it 'sends a welcome message' do
         verify(output).puts('Welcome to Codebreaker!')
@@ -16,6 +14,46 @@ module Codebreaker
       it 'prompts for the first guess' do
         verify(output).puts('Enter guess:')        
       end
+      
+      describe '#guess' do
+        context 'with no matches' do
+          before { game.guess('5555') }       
+          it 'sends a mark with ""' do            
+            verify(output).puts('')
+          end
+        end
+        
+        context 'with 1 number match' do
+          before { game.guess('2555') }
+          it 'sends a mark with "-"' do
+            verify(output).puts('-')
+          end          
+        end
+        
+        context 'with 1 exact match' do
+          before { game.guess('1555') }
+          it 'sends a mark with "+"' do
+            verify(output).puts('+')
+          end          
+        end
+        
+        context 'with 2 number matches' do
+          before { game.guess('2355') }
+          it 'sends a mark with "--"' do
+            verify(output).puts('--')
+          end          
+        end 
+        
+        context 'with 1 number match and 1 exact match (in that order)' do
+          before { game.guess('2535') }
+          it 'sends a mark with "+-"' do
+            verify(output).puts('+-')
+          end          
+        end       
+        
+      end
     end
+    
+
   end
 end
